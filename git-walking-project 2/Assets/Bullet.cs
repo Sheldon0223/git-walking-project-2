ad2277a935a8c216eb24149594a0d2e5fc93b556
Sheldon0223 <sheldonchang20030223@gmail.com>
2022-11-02 10:28:28 +0800

    bullet c# script change

--- git-walking-project 2/Assets/Bullet.cs
+++ git-walking-project 2/Assets/Bullet.cs
@@ -6,20 +6,19 @@ public class Bullet : MonoBehaviour
 {
     Rigidbody rb;
     float lifeTime = 0; // 計時秒數
-    private float speed = 800f;
+    private float speed = 800f ;
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         // 設定一個持續的方向力，往面對方向（forward = +z)
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.TransformDirection(Vector3.forward*speed);
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
        // 計時三秒後刪除
         lifeTime += Time.deltaTime;
         if (lifeTime > 3)
